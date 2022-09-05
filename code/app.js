0336df273ac541f48db667435a802e30b343b8c3
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-05 08:38:28 +0200

    Section 6

--- code/app.js
+++ code/app.js
@@ -1,1 +1,56 @@
-alert("It's working");
+
+
+
+
+
+// const animalPairs = [
+//     ['doe', 'buck'],
+//     ['ewe', 'ram'],
+//     ['peahen', 'peacock']
+// ];
+// console.log(animalPairs[1][0]);
+
+// const tablerotresenraya = [
+//     ['0', null, 'X'],
+//     [null, 'X', '0'],
+//     ['x', '0', null]
+// ];
+// console.log(tablerotresenraya);
+
+// console.log(animalPairs[0,1]);
+// console.log(animalPairs[0,2]);
+
+
+// const myEggs = ['white','grey'];
+// myEggs.push('violet');
+// console.log(myEggs);
+
+// console.log(myEggs.shift());
+// console.log(myEggs);
+
+// myEggs.unshift('brown');
+// console.log(myEggs);
+
+// console.log(myEggs.pop());
+// console.log(myEggs);
+
+// const misHuevos = []; 
+// myEggs = misHuevos
+
+// let diasSemana = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+// console.log(diasSemana);
+// let laborables = diasSemana.slice(0,5);
+// console.log(laborables);
+// let festivos = diasSemana.slice(-2);
+// console.log(festivos);
+
+
+// diasSemana.splice(3,2,'thursday', 'friday');
+// console.log(diasSemana);
+// diasSemana.sort();
+// console.log(diasSemana);
+// let colors = [`naranja`];
+// let miColor = colors;
+// miColor.push('azul');
+// console.log(colors);
+// console.log(miColor);
