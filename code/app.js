4c0d2a28e73fe687f411e86f1bd655aa02bf13f4
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-06 13:17:05 +0200

    Aplicar funciones a colecciones de datos

--- code/app.js
+++ code/app.js
@@ -2,6 +2,82 @@
 
 
 
+// const nums = [9,8,7,6,5,4,3,2,1];
+// const doubles  = nums.map(function(num){
+//     return num * 2;
+// })
+
+// const isEven = nums.map(function(n){
+//     return{
+//         value:n,
+//         isEven:n%2 ===0
+//     }
+// })
+
+
+
+// const texts = ['rofl', 'lol', 'omg', 'ttyl'];
+// const caps = texts.map(function (t) {
+//     return t.toUpperCase();
+// })
+
+// const separadoPoPuntos = texts.map(function (text){
+//     return text.toUpperCase().split('').join('.');
+// })
+
+// texts;
+// caps;
+
+// const books = [{
+//     title: 'Good Omens',
+//     authors: ['Terry Pratchett', 'Neil Gaiman'],
+//     rating: 4.25
+//   },
+//   {
+//     title: 'Bone: The Complete Edition',
+//     authors: ['Jeff Smith'],
+//     rating: 4.42
+//   },
+//   {
+//     title: 'American Gods',
+//     authors: ['Neil Gaiman'],
+//     rating: 4.11
+//   },
+//   {
+//     title: 'A Gentleman in Moscow',
+//     authors: ['Amor Towles'],
+//     rating: 4.36
+//   }
+// ]
+
+// const soloTitulos = books.map(function titulo(book) {
+//     return book.title;
+// })
+
+// // for (let book of books){
+// //     console.log(book.title.toUpperCase());
+// // }
+
+// books.forEach(function (book, index) {
+//     console.log(book.title.toUpperCase());     
+// })
+
+
+
+// const nums = [9,8,7,6,5,4,3,2,1];
+
+// nums.forEach(function (kiko) {
+//     console.log(kiko);
+// })
+
+// function printTriple(num){
+//     console.log(num*3);
+// }
+
+// nums.forEach(printTriple)
+
+
+
 // hoot();
 
 // function hoot (){
