bf11441f3a954454ab76a1e1a188ade21927a21a
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-07 12:29:45 +0200

    miscellaneous js features

--- code/app.js
+++ code/app.js
@@ -1,67 +1,72 @@
-const books = [{
-    title: 'Good Omens',
-    authors: ['Terry Pratchett', 'Neil Gaiman'],
-    rating: 4.25,
-    genres: ['fiction', 'fantasy']
-  },
-  {
-    title: 'Changing My Mind',
-    authors: ['Zadie Smith'],
-    rating: 3.83,
-    genres: ['nonfiction', 'essays']
-  },
-  {
-    title: 'Bone: The Complete Edition',
-    authors: ['Jeff Smith'],
-    rating: 4.42,
-    genres: ['fiction', 'graphic novel', 'fantasy']
-  },
-  {
-    title: 'American Gods',
-    authors: ['Neil Gaiman'],
-    rating: 4.11,
-    genres: ['fiction', 'fantasy']
-  },
-  {
-    title: 'A Gentleman in Moscow',
-    authors: ['Amor Towles'],
-    rating: 4.36,
-    genres: ['fiction', 'historical fiction']
-  },
-  {
-    title: 'The Name of the Wind',
-    authors: ['Patrick Rothfuss'],
-    rating: 4.54,
-    genres: ['fiction', 'fantasy']
-  },
-  {
-    title: 'The Overstory',
-    authors: ['Richard Powers'],
-    rating: 4.19,
-    genres: ['fiction', 'short stories']
-  },
-  {
-    title: 'The Way of Kings',
-    authors: ['Brandon Sanderson'],
-    rating: 4.65,
-    genres: ['fantasy', 'epic']
-  },
-  {
-    title: 'Lord of the flies',
-    authors: ['William Golding'],
-    rating: 3.67,
-    genres: ['fiction']
-  }
-]
+function multiply(x,y) {
+  if (typeof y === '')  
+  return x*y;
+}
 
-const miLibro = books.reduce((groupedBooks, book) => {
-  const key = book.genres[0];
-  if(!groupedBooks[key])
-    groupedBooks[key] = [];
-  groupedBooks[key].push(book);    
-  return groupedBooks;
-},
-{});
+// const books = [{
+//     title: 'Good Omens',
+//     authors: ['Terry Pratchett', 'Neil Gaiman'],
+//     rating: 4.25,
+//     genres: ['fiction', 'fantasy']
+//   },
+//   {
+//     title: 'Changing My Mind',
+//     authors: ['Zadie Smith'],
+//     rating: 3.83,
+//     genres: ['nonfiction', 'essays']
+//   },
+//   {
+//     title: 'Bone: The Complete Edition',
+//     authors: ['Jeff Smith'],
+//     rating: 4.42,
+//     genres: ['fiction', 'graphic novel', 'fantasy']
+//   },
+//   {
+//     title: 'American Gods',
+//     authors: ['Neil Gaiman'],
+//     rating: 4.11,
+//     genres: ['fiction', 'fantasy']
+//   },
+//   {
+//     title: 'A Gentleman in Moscow',
+//     authors: ['Amor Towles'],
+//     rating: 4.36,
+//     genres: ['fiction', 'historical fiction']
+//   },
+//   {
+//     title: 'The Name of the Wind',
+//     authors: ['Patrick Rothfuss'],
+//     rating: 4.54,
+//     genres: ['fiction', 'fantasy']
+//   },
+//   {
+//     title: 'The Overstory',
+//     authors: ['Richard Powers'],
+//     rating: 4.19,
+//     genres: ['fiction', 'short stories']
+//   },
+//   {
+//     title: 'The Way of Kings',
+//     authors: ['Brandon Sanderson'],
+//     rating: 4.65,
+//     genres: ['fantasy', 'epic']
+//   },
+//   {
+//     title: 'Lord of the flies',
+//     authors: ['William Golding'],
+//     rating: 3.67,
+//     genres: ['fiction']
+//   }
+// ]
+
+// const miLibro = books.reduce((groupedBooks, book) => {
+//   const key = book.genres[0];
+//   if(!groupedBooks[key])
+//     groupedBooks[key] = [];
+//   groupedBooks[key].push(book);    
+//   return groupedBooks;
+// },
+// {});
 
 
 // const votes = ['y', 'y', 'y', 'n', 'n', 'absent'];
